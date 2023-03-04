640a68a69d4ddca71d119bfe89406af1113e1187
sergey <xt-2000@mail.ru>
2023-03-05 00:33:45 +0300

    first comment

---
+++ Homework_1.java
@@ -0,0 +1,161 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Homework_1 {
+
+    public static void main(String[] args) {
+        System.out.println("# 1");
+        System.out.println(isSumBetween10And20(5, 15)); // true
+        System.out.println(isSumBetween10And20(7, 15)); // false
+
+        System.out.println("\n# 2");
+        System.out.println(isPositive(5)); // true
+        System.out.println(isPositive(-3)); // false
+         
+        System.out.println("\n# 3");
+        System.out.println(printString("abcd", 5)); // abcdabcdabcdabcdabcd
+
+        System.out.println("\n# 4");
+        System.out.println(isLeapYear(2023));
+        System.out.println(); 
+
+        System.out.println("\n# 5");
+        System.out.println(Arrays.toString(createArray(3, 3))); // 3, 3, 3, 3, 3
+        System.out.println(); 
+        
+      ///////////////// **
+
+        System.out.println("\n# 1**");
+        int [] arr = RandomArray(5);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(reversArray(arr)));
+
+        System.out.println("\n# 2**");
+        int [] arr2 = new int[] { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(correctArr(arr2)));
+        
+        System.out.println("\n# 3**");
+        int[][] array = new int[5][5]; // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, x], [0, 0, 0, 1]]
+        //System.out.println(Arrays.toString(array));
+        for(int i = 0; i < array.length; i++ ){
+            for(int k = 0; k < array[i].length; k++ ){
+                if (i == k){
+                    array[i][k]=1;
+                }
+            }
+        }
+        for (int[] ints : array) {
+            System.out.println(Arrays.toString(ints));
+        }
+        
+
+
+
+    }
+    
+    /**
+     * 1. Написать метод, принимающий на вход два целых числа и проверяющий,
+     * что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
+     */
+    private static boolean isSumBetween10And20(int a, int b) {
+        // проверить, что сумма a и b лежит между 10 и 20
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20 ){
+            return true;
+        }
+        return false;
+    }
+    
+    
+    private static boolean isPositive(int x) {
+        // проверить, что х положительное
+        if (x > 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    
+    private static String printString(String source, int repeat) {
+        // напечатать строку source repeat раз
+        String result = "";
+        for( int u = 0; u < repeat; u++ ) {
+            result = result + source;
+        }
+        return result;
+    }
+    
+    
+    private static boolean isLeapYear(int year) {
+        // проверить, является ли год високосным. если да - return true
+        if (year % 4 == 0|| year % 400 ==0 ){
+            return true;
+        }
+        return false;
+    }
+    
+    
+    private static int[] createArray(int len, int initalValue) {
+        // должен вернуть массив длины len, каждое значение которого равно initialValue
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++){
+            arr[i] = initalValue;
+        }
+
+        return arr;
+    }
+    
+    
+    /**
+     * 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+     * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     * 2. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+     * 3. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+     * и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно). 
+     * Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+     * * Также заполнить элементы побочной диагонали
+     */
+    
+    
+    private static int[] RandomArray(int lenArray){
+        int[] list = new int  [lenArray];
+        Random rnd = new Random();
+        
+        for (int i = 0; i < lenArray; i++)
+        {
+            list[i] = rnd.nextInt(2 );
+        }
+       return list;
+    }
+
+
+    private static int[] reversArray(int[] array){
+        int len = array.length;
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++){
+            if(array[i] == 1){
+                res[i] = 0;
+            }
+            else{
+                res[i] = 1;
+            }
+        }
+    return res;
+    }
+
+
+    private static int[] correctArr(int[] array){
+        int len = array.length;
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++){
+            if(array[i] < 6){
+                res[i] = array[i]*2;
+            }
+            else{
+                res[i] = array[i];
+            }
+        }
+    return res;
+    }
+}
